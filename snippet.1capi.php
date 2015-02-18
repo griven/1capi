5c2960e8f262b5f5eaceead3322a5681fb34e0cf
griven <vadim.r@fabricasaitov.ru>
2015-02-18 11:50:21 +0300

    rc1

--- snippet.1capi.php
+++ snippet.1capi.php
@@ -1,6 +1,9 @@
 <?php
+// константы которые потом можно перенести в конфиг
 define ("CATALOG_ID", 14);
 define ("TV_MIN_INFO", 0);
+define ("PRODUCT_TEMPLATE", 4);
+define ("START_DEL_ID", 43);
 
 $exchange = new Exchange($modx);
 
@@ -10,6 +13,8 @@ class Exchange{
   public $cmd;
   public $data;
   public $sig;
+  
+  public $result;
   
   function __construct(modX &$modx) {
     $this->modx = &$modx;
@@ -21,8 +26,11 @@ class Exchange{
   */
   function getPostData() {
     $this->cmd = $_POST['cmd'];
-    $this->data = json_decode($_POST['data']);
+    $this->data = json_decode($_POST['data'], true);
     $this->sig   = $_POST['sig'];
+    
+    if($this->data == null) echo "null data";
+    print_r($this->data);
   }
   
   /**
@@ -42,13 +50,41 @@ class Exchange{
         $this->delProduct();
         break;
       case 'putProduct':
-        $this->putProduct();
+        $this->putProduct(false);
+        break;
+      case 'putProductCategory':
+        $this->putProduct(true);
+        break;
+      case 'getCategories':
+        $this->getCategories();
+        break;
+      default:
+        $this->result["error"] .= "|command not found";
         break;
     }
+    
+    $this->response();
+  }
+  
+  /**
+  * Функция формирующая ответ JSON
+  */
+  function response() {
+    if(count($this->result) == 0) {
+      $this->result["error"] .= "|empty result";
+    }
+    echo "result\n".json_encode($this->result);
+    echo "\n\npretty result\n";
+    foreach($this->result as $res) {
+      echo json_encode($res)."\n";
+    }
+    // иначе неправильно отдается JSON с большой вложенностью (например getProduct)
+    exit; 
   }
   
   /**
   * Функция получения товаров
+  * если в json не указан id, то получает все товары корневого каталога
   */
   function getProduct() {
     $ids = $this->getIdsFromData();
@@ -65,14 +101,33 @@ class Exchange{
         array_push($result,$productInfo);
       }
     }
-    print_r($result);
+    
+    $this->result = $result;
+  }
+  
+  /**
+  * Функция получения категорий товаров
+  */
+  function getCategories() {
+    $ids = $this->modx->getChildIds(CATALOG_ID);
+    
+    $result = array();
+    
+    foreach($ids as $id) {
+      $productInfo = $this->getProductInfo($id);
+      if(isset($productInfo[0]["isfolder"]) && $productInfo[0]["isfolder"] == 1) {
+        array_push($result,$productInfo);
+      }
+    }
+    
+    $this->result = $result;
   }
   
   /**
   * Получает всех детей базового каталога
   */
   function getAllChild() {
-    print_r($this->modx->getTree(CATALOG_ID));
+    $this->result = $this->modx->getTree(CATALOG_ID);
   }
   
   /**
@@ -101,7 +156,6 @@ class Exchange{
     if($resource) {
       $result = $resource->toArray();
     } else {
-      echo "not found $id";
       $result = null;
     }
     return $result;
@@ -129,75 +183,102 @@ class Exchange{
         array_push($result, $tv_param);
       }
     } else {
-      echo "not found $id";
       $result = null;
     }
     return $result;
   }
   
-  function putProduct() {
+  /**
+  * Создает или обновляет ресурс
+  * @param $isCategory - создать категорию или же просто товар
+  */
+  function putProduct($isCategory = false) {
     $ids = $this->getIdsFromData();
+    
     if($ids == null) {
-      $this->createResource();
+      $this->createResource($isCategory);
     } else {
-      $this->updateResource($ids);
+      //$this->updateResource($ids);
     }
   }
   
-  function createResource() {
-    $parent = 15;
-    $template = 4;
-    $publishedon =  date('Y-m-d H:i:s');
-    $newResource = $this->modx->newObject('modResource');
-    $newResource->set('pagetitle',$publishedon);
-    $newResource->set('parent',$parent);
-    $newResource->set('template',$template);
-    $newResource->set('published',1);
-    $newResource->set('show_in_tree',0);
-    $newResource->set('publishedon',$publishedon);
-    $newResource->save();
-    $id = $newResource->get('id');
-    $newResource->set('pagetitle','test page '.$id);
-    $newResource->save();
-    echo "new id is $id\n";
+  /**
+  * Создает новый ресурс с параметрами взятыми из JSON
+  */
+  function createResource($isCategory) {
+    if(array_key_exists('pagetitle',$this->data) && array_key_exists('parent',$this->data)) {
+      $newResource = $this->modx->newObject('modResource');
+      
+      // стандартные значения
+      $newResArray = $newResource->toArray();
+      $newResArray['template'] = PRODUCT_TEMPLATE;
+      $newResArray['published'] = 1;
+      $newResArray['publishedon'] = date('Y-m-d H:i:s');
+      $newResArray['show_in_tree'] = 0;
+      $newResArray['isfolder'] = ($isCategory) ? 1 : 0;
+     
+     // значения полученные из JSON
+      foreach($this->data as $key=>$value) {
+        if(array_key_exists($key, $newResArray)) {
+          $newResArray[$key] = $value;
+        }
+      }
+      
+      // сохраненяем ресурс и получяем id
+      $newResource->fromArray($newResArray);
+      $newResource->save();
+      $id = $newResource->get('id');
+      $this->result = "$id";
+    } else {
+      $this->result["error"] .= "|pagetitle or parent not found";
+    }
   }
   
   function delProduct() {
-    echo "delProduct\n";
     $ids = $this->getIdsFromData();
-    if($ids==null) $ids= array();
-    foreach($ids as $id) {
-      $this->removeResource($id);
-    }
-  }
-  
-  function removeResource($id) {
-    echo "removeResource $id!\n";
-    // условие чисто для разработки (чтобы лишнее не поудалять)
-    if( $id > 43) { 
-      $resource = $this->modx->getObject('modResource', $id);
-      if($resource == null) {
-        echo "resource not found";
-      } else if ($resource->remove() == false) {
-        echo 'An error occurred while trying to remove the box!';
+    if(is_array($ids)) {
+      foreach($ids as $id) {
+        $this->removeResource($id);
       }
     }
   }
   
-  function getIdsFromData() {
-    echo "getIdsFromData\n";
-    $ids = array();
-    if(isset($this->data->id)) {
-      if(is_array($this->data->id)) {
-        $ids = $this->data->id;
+  /**
+  * удаляет ресурс по его id
+  * @param $id - номер ресурса
+  */
+  function removeResource($id) {
+    // условие чисто для разработки (чтобы лишнее не поудалять)
+    if( $id > START_DEL_ID) {
+      $resource = $this->modx->getObject('modResource', $id);
+      if($resource == null) {
+        $this->result["error"] .= "|resource not found";
+      } else if ($resource->remove() == false) {
+        $this->result["error"] .= '|An error occurred while trying to remove the box!';
       } else {
-        array_push($ids,$this->data->id);
+        $this->result = "$id deleted";
+      }
+    }
+  }
+  
+  /**
+  * Получает id в виде массива из JSON
+  * @return массив id или null
+  */
+  function getIdsFromData() {
+    $ids = array();
+    if(isset($this->data['id'])) {
+      if(is_array($this->data['id'])) {
+        $ids = $this->data['id'];
+      } else {
+        array_push($ids,$this->data['id']);
       }
     } else {
       $ids = null;
     }
     return $ids;
   }
+  
   
   function updateResource($ids) {
     if(!is_array($ids)) return;
