1823da420dd4d4a77d83fdaa60865bea248861d5
griven <griveh@yandex.ru>
2015-07-14 18:15:55 +0300

    package version

---
+++ _build/build.transport.php
@@ -0,0 +1,59 @@
+<?php
+$tstart = explode(' ', microtime());
+$tstart = $tstart[1] + $tstart[0];
+set_time_limit(0);
+
+/* define package names */
+define('PKG_NAME','1capi');
+define('PKG_NAME_LOWER','1capi');
+define('PKG_VERSION','1.0.0');
+define('PKG_RELEASE','pl');
+
+/* define build paths */
+$root = dirname(dirname(__FILE__)).'/';
+$sources = array(
+    'root' => $root,
+    'build' => $root . '_build/',
+    'resolvers' => $root . '_build/data/resolvers.php',
+    'lexicon' => $root . 'core/components/'.PKG_NAME_LOWER.'/lexicon/',
+    'docs' => $root.'core/components/'.PKG_NAME_LOWER.'/docs/',
+    'source_core' => $root.'core/components/'.PKG_NAME_LOWER,
+);
+unset($root);
+
+/* override with your own defines here (see build.config.sample.php) */
+define('MODX_CORE_PATH', dirname(dirname(__FILE__)).'/core/');
+require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+
+$modx= new modX();
+$modx->initialize('mgr');
+echo '<pre>'; /* used for nice formatting of log messages */
+$modx->setLogLevel(modX::LOG_LEVEL_INFO);
+$modx->setLogTarget('ECHO');
+
+$modx->loadClass('transport.modPackageBuilder','',false, true);
+$builder = new modPackageBuilder($modx);
+$builder->createPackage(PKG_NAME_LOWER,PKG_VERSION,PKG_RELEASE);
+$builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
+
+/* resolve files */
+$vehicle = include $sources["resolvers"];
+$builder->putVehicle($vehicle);
+
+/* now pack in the license file, readme and setup options */
+$builder->setPackageAttributes(array(
+    'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
+    'license' => file_get_contents($sources['docs'] . 'license.txt'),
+    'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
+));
+$modx->log(modX::LOG_LEVEL_INFO,'Added package attributes');
+
+/* zip up package */
+$modx->log(modX::LOG_LEVEL_INFO,'Packing up transport package zip...');
+$builder->pack();
+
+$tend= explode(" ", microtime());
+$tend= $tend[1] + $tend[0];
+$totalTime= sprintf("%2.4f s",($tend - $tstart));
+$modx->log(modX::LOG_LEVEL_INFO,"\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
+exit ();
